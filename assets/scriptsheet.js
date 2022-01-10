27f9c80134b392eb02d1ab0e0d101933469f073e
J.Cole <ccjohn015@gmail.com>
2022-01-09 20:06:37 -0600

    added choice answers

---
+++ assets/scriptsheet.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+var quizQuestions = [
+    {
+        question: "How do you create a function in JavaScript?",
+        choice1: "function()",
+        choice2: "create myFunction = function()",
+        choice3: "var myFunction = function()",
+        choice4: "function.create",
+        answer: 3
+    },
+    {
+        question: "What is the method that converts a JavaScript object or value into a string?",
+        choice1: "JSON.stringify()",
+        choice2: "make.string()",
+        choice3: "JSON.parse()",
+        choice4: "stringify()",
+        answer: 1
+    },
+    {
+        question: "Where does the script tag go in the HTML file? ",
+        choice1: "<head> section",
+        choice2: "<body> section ",
+        choice3: "Both <head> or <body> section",
+        choice4: "<footer> section",
+        answer: 3
+    },
+    {
+        question: "Which of the below choices can be objects?",
+        choice1: "Booleans",
+        choice2: "Numbers",
+        choice3: "Strings",
+        choice4: "All of the above",
+        answer: 4
+    }
+];
